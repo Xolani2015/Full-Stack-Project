28edf20de1bb4b51b4356e195bd7d622a0286fdb
Xolani <xr.mkhwanazi.20@gmail.com>
2021-06-05 19:19:06 +0200

    styling table

--- react-application/src/components/Tweetlist.js
+++ react-application/src/components/Tweetlist.js
@@ -1,14 +1,62 @@
 
+import React from 'react';
+import * as ReactBootStrap from 'react-bootstrap';
 
-import React from 'react'
+
+
+
+
 
 function Tweetlist(props) {
 
   const updateTweet = (tweet) => {
      props.updateTweet(tweet)
+
+
   }
+
+
     return (
         <div>
+
+
+<ReactBootStrap.Table class="content-table">
+  <thead>
+    <tr  >
+      <th scope="col">#</th>
+      <th scope="col">@User</th>
+      <th scope="col">Post</th>
+      <th scope="col">Sentiment</th>
+      <th scope="col">Operations</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+      props.tweets && props.tweets.map((item) => (
+         <tr key={item.id} class="table-warning">
+      <th scope="row" >{item.id}</th>
+      <td>{item.tweetuser}</td>
+      <td>{item.tweettext}</td>
+      <td>{item.tweetsentiment}</td>
+      <td> 
+                <div className = "row">
+                  <div className = "col">
+                    <button className = "btn btn-primary"
+                    onClick = {() => updateTweet(item)}
+                    >Update</button>
+                  </div>
+                 
+                 <div className = "col">
+                   <button className = "btn btn-danger">Delete</button>
+                 </div>
+                 </div></td>
+    </tr>
+      ))
+    }
+   
+   
+  </tbody>
+</ReactBootStrap.Table>
             
                {props.tweets && props.tweets.map(tweet => {
              return(
@@ -18,17 +66,6 @@ function Tweetlist(props) {
                  <p>{tweet.tweetdate}</p>
                  <p>{tweet.tweetsentiment}</p>
   
-                <div className = "row">
-                  <div className = "col-md-1">
-                    <button className = "btn btn-primary"
-                    onClick = {() => updateTweet(tweet)}
-                    >Update</button>
-                  </div>
-                 
-                 <div className = "col">
-                   <button className = "btn btn-danger">Delete</button>
-                 </div>
-                 </div>
                  <hr/>
           </div>
           )
